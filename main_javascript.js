3bebd9c54ee1a43cb8934af7e46252732c96c736
maksymkobets <kobmax@gmail.com>
2017-02-28 21:16:14 +0200

    q
    q

---
+++ main_javascript.js
@@ -0,0 +1,69 @@
+function initMap() {
+
+    var myLatLng = {lat: 50.618641, lng: 26.258646};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        scrollwheel: false,
+        zoom: 18
+    });
+
+    var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h4 id="firstHeading" class="firstHeading">Адреса:</h4>'+
+      '<div id="bodyContent">'+
+      '<h6>вул.Патріса Лумумбы, 87</h6>'+
+      '</div>'+
+      '<h4 id="firstHeading" class="firstHeading">Телефони:</h4>'+
+      '<div id="bodyContent">'+
+      '<h6>099 221 71 73</h6>'+
+      '<h6>063 556 66 44</h6>'+
+      '</div>'+
+      '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+    content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: ' '
+    });
+    marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+infowindow.open(map,marker);
+}
+
+
+   
+
+
+
+
+
+
+
+function hover(element) {
+    element.setAttribute('src', 'ashot.jpg');
+}
+function unhover(element) {element.setAttribute('src', 'elena.jpg');
+}
+
+function hover1(element) {
+    element.setAttribute('src', 'ashot2.jpg');
+}
+function unhover1(element) {
+    element.setAttribute('src', 'elena.jpg');
+}
+
+function hover2(element) {
+    element.setAttribute('src', 'ashot3.jpg');
+}
+function unhover2(element) {
+    element.setAttribute('src', 'elena.jpg');
+}
+
